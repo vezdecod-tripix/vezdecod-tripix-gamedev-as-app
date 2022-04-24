4edc8437b6a547dfa8924c8ed2cb1bff4614d48c
cclarice <179danil@gmail.com>
2022-04-24 09:27:10 +0300

    second game done

--- src/panels/Balancer.js
+++ src/panels/Balancer.js
@@ -1,26 +1,383 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import PropTypes from 'prop-types'
 
-import { Panel, PanelHeader, PanelHeaderBack } from '@vkontakte/vkui'
+import { Button, Div, Panel, PanelHeader, PanelHeaderBack } from '@vkontakte/vkui'
 
-import what from '../img/what.svg'
+import lever from '../img/lever.svg'
+import triangle from '../img/triangle.svg'
+import frogImg from '../img/frog.svg'
+import catImg from '../img/cat.svg'
 import './Balancer.css'
 
-const Balancer = props => (
-	const [modalVisible, setModelVisible] = useState(false)
-	<Panel id={props.id}>
-		<PanelHeader
-			left={<PanelHeaderBack onClick={props.go} data-to="home" />}
-		>
-			Балансер пароля
-		</PanelHeader>
-		<img className="Persik" src={what} />
-	</Panel>
-)
+const angles = [40, 30, 20, 10, 0, -10, -20, -30, -40]
+
+const cat = <img
+  src={ catImg }
+  alt="cat"
+/>
+const frog = <img
+  src={ frogImg }
+  alt="frog"
+/>
+
+const symbols = '!@.-_'
+const digits = '0123456789'
+const words = ['cat', 'frog', 'bear', 'fox', 'dog', 'owl']
+const year = ['2001', '1996', '1964', '2004', '2006']
+const letters = 'abcdefgklmnprstz'
+const defaultPassword = 'p@$$w0rd'
+
+const anyFrom = (from) => {
+  const max = from.length - 1
+
+  return from[Math.floor(Math.random() * max)]
+}
+
+const Balancer = props => {
+  const [score, setScore] = useState(0)
+  const [password, setPassword] = useState(defaultPassword)
+  let copyPassword = password
+  const addToStart = (add) => {
+    copyPassword = add + copyPassword
+    setPassword(copyPassword)
+    setAll()
+  }
+  const addToEnd = (add) => {
+    copyPassword = copyPassword + add
+    setPassword(copyPassword)
+    setAll()
+  }
+  const addToMiddle = (add) => {
+    copyPassword = insertToCenter(copyPassword, anyFrom(letters))
+    setPassword(copyPassword)
+    setAll()
+  }
+  const sedPassword = (newPassword) => {
+    console.log('sedPassword:', newPassword)
+    copyPassword = newPassword
+    setPassword(copyPassword)
+    setAll()
+  }
+
+  const setAll = () => {
+    setAdder(additions[Math.floor(Math.random() * (additions.length - 1))])
+    setRemover(removers[Math.floor(Math.random() * (removers.length - 1))])
+    setSome(calcMemorize(), calcSecurity())
+  }
+  const calcMemorize = () => {
+    let amount = 5
+
+    if (copyPassword.length > 15) {
+      amount--;
+    }
+    if (copyPassword.length > 12) {
+      amount--;
+    }
+    if (copyPassword.length > 8) {
+      amount--;
+    }
+    if ((copyPassword.split(/[!|@|.|_|-]/)).length > 5) {
+      amount--;
+    }
+    if (copyPassword.match(/\d/)?.length > 6) {
+      amount--;
+    }
+
+    setMemorize(amount)
+    return amount
+  }
+  const calcSecurity = () => {
+    let amount = 5
+
+    if (copyPassword.length < 12) {
+      amount--;
+    }
+    if (copyPassword.length < 8) {
+      amount--;
+    }
+    if (!/[A-Z]/.test(copyPassword)) {
+      amount--;
+    }
+    if (!(copyPassword.match(/[!|@|.|_|-]/))) {
+      amount--;
+    }
+    if (!(copyPassword.match(/\d/))) {
+      amount--;
+    }
+
+    setSecurity(amount)
+    return amount
+  }
+  const additions = [
+    {
+      text: 'Добавить букву в начало', action: () => addToStart(anyFrom(letters))
+    },
+    { text: 'Добавить букву в конец', action: () => addToEnd(anyFrom(letters)) },
+     { text: 'Добавить букву в середину', action: () => addToMiddle(anyFrom(letters)) },
+     {
+     text: 'Добавить заглавную букву в начало',
+     action: () => addToStart(anyFrom(letters).toUpperCase())
+     },
+     {
+     text: 'Добавить заглавную букву в конец',
+     action: () => addToEnd(anyFrom(letters).toUpperCase())
+     },
+     {
+     text: 'Добавить заглавную букву в середину',
+     action: () => addToMiddle(anyFrom(letters).toUpperCase())
+     },
+     { text: 'Добавить слово в начало', action: () => addToStart(anyFrom(words)) },
+     { text: 'Добавить слово в конец', action: () => addToEnd(anyFrom(words)) },
+     { text: 'Добавить слово в середину', action: () => addToMiddle(insertToCenter(anyFrom(words))) },
+     { text: 'Добавить цифру в начало', action: () => addToStart(anyFrom(digits)) },
+     { text: 'Добавить цифру в конец', action: () => addToEnd(anyFrom(digits)) },
+     { text: 'Добавить цифру в середину', action: () => addToMiddle(insertToCenter(anyFrom(digits))) },
+     { text: 'Добавить символ в начало', action: () => addToStart(anyFrom(symbols)) },
+     { text: 'Добавить символ в конец', action: () => addToEnd(anyFrom(symbols)) },
+     { text: 'Добавить символ в середину', action: () => addToMiddle(insertToCenter(anyFrom(symbols))) },
+     { text: 'Добавить год в начало', action: () => addToStart(anyFrom(year)) },
+     { text: 'Добавить год в конец', action: () => addToEnd(anyFrom(year)) },
+     { text: 'Добавить год в середину', action: () => addToMiddle(insertToCenter(anyFrom(year))) }
+  ]
+
+  const lose = () => {
+    if (props.bestScore < score) {
+      localStorage.setItem('worstPasswordsTripixPopularityBestScore', score)
+      props.setBestScore(score)
+    }
+    props.go('home')
+  }
+
+  const removers = [
+    {
+      text: 'Удалить 1 элемента в начале', action: () => {
+        sedPassword(copyPassword.slice(-copyPassword.length + 1))
+      }
+    },
+    { text: 'Удалить 2 элемента в начале', action: () => sedPassword(copyPassword.slice(-copyPassword.length + 2)) },
+     { text: 'Удалить 3 элемента в начале', action: () => sedPassword(copyPassword.slice(-copyPassword.length + 3)) },
+     { text: 'Удалить 4 элемента в начале', action: () => sedPassword(copyPassword.slice(-copyPassword.length + 4)) },
+     { text: 'Удалить 1 элемента в конце', action: () => sedPassword(copyPassword.slice(0, copyPassword.length - 1)) },
+     { text: 'Удалить 2 элемента в конце', action: () => sedPassword(copyPassword.slice(0, copyPassword.length - 2)) },
+     { text: 'Удалить 3 элемента в конце', action: () => sedPassword(copyPassword.slice(0, copyPassword.length - 3)) },
+     { text: 'Удалить 4 элемента в конце', action: () => sedPassword(copyPassword.slice(0, copyPassword.length - 4)) },
+     { text: 'Удалить все маленькие буквы', action: () => sedPassword(copyPassword.split('').filter((char) => !/[a-z]/.test(char)).join('')) },
+     { text: 'Удалить все заглавные буквы', action: () => sedPassword(copyPassword.split('').filter((char) => !/[A-Z]/.test(char)).join('')) },
+     { text: 'Удалить все цифры меньше 5', action: () => sedPassword(copyPassword.split('').filter((char) => !/[0-4]/.test(char)).join('')) },
+     { text: 'Удалить все цифры больше 4', action: () => sedPassword(copyPassword.split('').filter((char) => !/[5-9]/.test(char)).join('')) },
+     { text: 'Удалить все цифры', action: () => sedPassword(copyPassword.split('').filter((char) => !/[0-9]/.test(char)).join('')) },
+     { text: 'Удалить все символы', action: () => sedPassword(copyPassword.split('').filter((char) => !/[!@.-_]/.test(char)).join('')) },
+  ]
+  const [security, setSecurity] = useState(2)
+  const [memorize, setMemorize] = useState(2)
+  const [angle, setAngle] = useState({ transform: `rotate(${ angles[4 + memorize - security] }deg)` })
+  const [cats, setCats] = useState([<img
+    src={ catImg }
+    alt="cat"
+    key="0"
+  />, <img
+    src={ catImg }
+    alt="cat"
+    key="1"
+  />])
+  const [frogs, setFrogs] = useState([<img
+    src={ frogImg }
+    alt="frog"
+    key="0"
+  />, <img
+    src={ frogImg }
+    alt="frog"
+    key="1"
+  />])
+  const [catsStyles, setCatsStyles] = useState({ bottom: `${ (angle - 2) * 10 }px` })
+  const [frogsStyles, setFrogsStyles] = useState({ bottom: `${ (angle - 2) * 10 }px` })
+  const [adder, setAdder] = useState(additions[Math.floor(Math.random() * (additions.length - 1))])
+  const [remover, setRemover] = useState(removers[Math.floor(Math.random() * (removers.length - 1))])
+
+  console.log(Math.floor(Math.random() * (additions.length - 1)))
+
+  const insertToCenter = (string, insert) => {
+    return [string.slice(0, Math.ceil((string.length) / 2)), insert, string.slice(-(string.length) / 2)].join('')
+  }
+
+  const setSome = (memorize, security) => {
+    const some = memorize - security
+
+    setCatsStyles({ bottom: `${ (some) * 10 }px` })
+    setFrogsStyles({ bottom: `${ (some) * -10 }px` })
+    const newCats = []
+    const newFrogs = []
+    for (let i = 0; i < security; i++) {
+      newCats.push(<img
+        src={ catImg }
+        alt="cat"
+        key={ i }
+      />)
+    }
+    for (let i = 0; i < memorize; i++) {
+      newFrogs.push(<img
+        src={ frogImg }
+        alt="frog"
+        key={ i }
+      />)
+    }
+    setFrogs(newFrogs)
+    setCats(newCats)
+    setAngle({ transform: `rotate(${ angles[4 + some] }deg)` })
+
+    if (Math.abs(some) >= 4) {
+      lose()
+    } else {
+      setScore(score + 1)
+    }
+  }
+
+  const increaseSecurity = () => {
+    const some = 4 + memorize - (security + 1)
+    const newCats = []
+    setSome(memorize, security + 1)
+
+    for (let i = 0; i < security + 1; i++) {
+      newCats.push(<img
+        src={ catImg }
+        alt="cat"
+        key={ i }
+      />)
+    }
+
+    setCats(newCats)
+    setSecurity(security + 1)
+    setAngle({ transform: `rotate(${ angles[some] }deg)` })
+  }
+
+  const decreaseSecurity = () => {
+    const some = 4 + memorize - (security - 1)
+    const newCats = []
+    setSome(memorize, security - 1)
+
+    for (let i = 0; i < security - 1; i++) {
+      newCats.push(<img
+        src={ catImg }
+        alt="cat"
+        key={ i }
+      />)
+    }
+
+    setCats(newCats)
+    setSecurity(security - 1)
+    setAngle({ transform: `rotate(${ angles[some] }deg)` })
+  }
+
+  const increaseMemorize = () => {
+    const some = 4 + (memorize + 1) - security
+    const newFrogs = []
+    setSome(memorize + 1, security)
+
+    for (let i = 0; i < memorize + 1; i++) {
+      newFrogs.push(<img
+        src={ frogImg }
+        alt="frog"
+        key={ i }
+      />)
+    }
+
+    setFrogs(newFrogs)
+    setMemorize(memorize + 1)
+    setAngle({ transform: `rotate(${ angles[some] }deg)` })
+  }
+
+  const decreaseMemorize = () => {
+    const some = 4 + (memorize - 1) - security
+    const newFrogs = []
+    setSome(memorize - 1, security)
+
+    for (let i = 0; i < memorize - 1; i++) {
+      newFrogs.push(<img
+        src={ frogImg }
+        alt="frog"
+        key={ i }
+      />)
+    }
+
+    setFrogs(newFrogs)
+    setMemorize(memorize + 1)
+    setAngle({ transform: `rotate(${ angles[some] }deg)` })
+  }
+
+  const clickHandler = (_action) => {
+    const action = _action
+    return () => {
+      action()
+    }
+  }
+
+  return (
+    <Panel id={ props.id }>
+      <PanelHeader
+        left={ <PanelHeaderBack
+          onClick={ props.go }
+          data-to="home"
+        /> }
+      >
+        Балансер пароля
+      </PanelHeader>
+      <span>
+				Счёт: { score }
+			</span>
+      <Div className="BalancerPassword">
+        { password }
+      </Div>
+      <Div className="Lever">
+        <Div
+          className="Cats"
+          style={ catsStyles }
+        >
+          { cats }
+        </Div>
+        <Div
+          className="Frogs"
+          style={ frogsStyles }
+        >
+          { frogs }
+        </Div>
+        <span>
+          <span>{ memorize }</span> : <span>{ security }</span>
+        </span>
+        <img
+          style={ angle }
+          className="LeverLever"
+          src={ lever }
+          alt="lever"
+        />
+        <img
+          className="LeverTriangle"
+          src={ triangle }
+          alt="triangle"
+        />
+      </Div>
+      <Div className="Actions">
+        <Button
+          onClick={ adder.action }
+          stretched
+          size="m"
+        >
+          { adder ? adder.text : 'error' }
+        </Button>
+        <Button
+          onClick={ remover.action }
+          stretched
+          size="m"
+        >
+          { remover ? remover.text : 'error' }
+        </Button>
+      </Div>
+    </Panel>
+  )
+}
 
 Balancer.propTypes = {
-	id: PropTypes.string.isRequired,
-	go: PropTypes.func.isRequired,
+  id: PropTypes.string.isRequired,
+  go: PropTypes.func.isRequired
 }
 
 export default Balancer
--- src/panels/Popularity.js
+++ src/panels/Popularity.js
@@ -12,138 +12,152 @@ import './Global.css'
 import data_passwords from '../data/passwords.js'
 
 const shuffle = (array) => {
-	let currentIndex = array.length, randomIndex;
+  let currentIndex = array.length, randomIndex
 
-	while (currentIndex !== 0) {
-		randomIndex = Math.floor(Math.random() * currentIndex);
-		currentIndex--;
+  while (currentIndex !== 0) {
+    randomIndex = Math.floor(Math.random() * currentIndex)
+    currentIndex--;
 
-		[array[currentIndex], array[randomIndex]] = [
-			array[randomIndex], array[currentIndex]];
-	}
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex], array[currentIndex]]
+  }
 
-	return array;
+  return array
 }
 let passwords = shuffle(Object.entries(data_passwords))
 
 const Popularity = props => {
-	const [score, setScore] = useState(0)
-	const [pointer, setPointer] = useState(1)
-	const [currentPassword, setCurrentPassword] = useState(passwords[pointer][0])
-	const [currentPasswordUses, setCurrentPasswordUses] = useState(passwords[pointer][1])
-	const [nextPassword, setNextPassword] = useState(passwords[pointer + 1][0])
-	const [nextPasswordUses, setNextPasswordUses] = useState(passwords[pointer + 1][1])
-	const [nextMask, setNextMask] = useState('???')
-	const [image, setImage] = useState(think)
-	const [backgroundColor, setBackgroundColor] = useState('#FFFFFF')
-	let alive = true
+  const [score, setScore] = useState(0)
+  const [pointer, setPointer] = useState(1)
+  const [currentPassword, setCurrentPassword] = useState(passwords[pointer][0])
+  const [currentPasswordUses, setCurrentPasswordUses] = useState(passwords[pointer][1])
+  const [nextPassword, setNextPassword] = useState(passwords[pointer + 1][0])
+  const [nextPasswordUses, setNextPasswordUses] = useState(passwords[pointer + 1][1])
+  const [nextMask, setNextMask] = useState('???')
+  const [image, setImage] = useState(think)
+  const [backgroundColor, setBackgroundColor] = useState('#FFFFFF')
+  const [disabled, setDisabled] = useState(false)
+  let alive = true
 
-	const lose = () => {
-		if (props.bestScore < score) {
-			localStorage.setItem('worstPasswordsTripixPopularityBestScore', score)
-			props.setBestScore(score)
-		}
-		props.go('home')
-		passwords = shuffle(passwords)
-	}
+  const lose = () => {
+    if (props.bestScore < score) {
+      localStorage.setItem('worstPasswordsTripixPopularityBestScore', score)
+      props.setBestScore(score)
+    }
+    props.go('home')
+    passwords = shuffle(passwords)
+  }
 
-	const point = () => {
-		setScore(score + 1)
-		setCurrentPassword(passwords[pointer + 1][0])
-		setCurrentPasswordUses(passwords[pointer + 1][1])
-		setNextPassword(passwords[pointer + 2][0])
-		setNextPasswordUses(passwords[pointer + 2][1])
-		setNextMask('???')
-		setImage(think)
-		setBackgroundColor( '#FFFFFF')
-	}
+  const point = () => {
+    setScore(score + 1)
+    setCurrentPassword(passwords[pointer + 1][0])
+    setCurrentPasswordUses(passwords[pointer + 1][1])
+    setNextPassword(passwords[pointer + 2][0])
+    setNextPasswordUses(passwords[pointer + 2][1])
+    setNextMask('???')
+    setImage(think)
+    setBackgroundColor('#FFFFFF')
+    setDisabled(false)
+  }
 
-	const counter = (to, count = 0) => {
-		if (count < to) {
-			count += Math.max((to - count) / 8, 1)
-			setNextMask(Math.floor(count))
-			requestAnimationFrame(() => counter(to, count))
-		} else {
-			setPointer(pointer < 195 ? pointer + 1 : 1)
-			setNextMask(nextPasswordUses)
-			setImage(alive ? cool : rage)
-			setBackgroundColor( alive ? '#D7FFE2' : '#FD7575')
-			setTimeout(() => {
-				alive ? point() : lose()
-			}, 1000)
-		}
-	}
+  const counter = (to, count = 0) => {
+    if (count < to) {
+      count += Math.max((to - count) / 8, 1)
+      setNextMask(Math.floor(count))
+      requestAnimationFrame(() => counter(to, count))
+    } else {
+      setPointer(pointer < 195 ? pointer + 1 : 1)
+      setNextMask(nextPasswordUses)
+      setImage(alive ? cool : rage)
+      setBackgroundColor(alive ? '#D7FFE2' : '#FD7575')
+      setTimeout(() => {
+        alive ? point() : lose()
+      }, 1000)
+    }
+  }
 
 
-	const selectCurrent = () => {
-		setBackgroundColor( '#EEEEEE')
-		setImage(wait)
-		console.log('current', currentPassword)
-		if (currentPasswordUses < nextPasswordUses) {
-			alive = false
-		}
-		counter(nextPasswordUses)
-	}
+  const selectCurrent = () => {
+    setDisabled(true)
+    setBackgroundColor('#EEEEEE')
+    setImage(wait)
+    console.log('current', currentPassword)
+    if (currentPasswordUses < nextPasswordUses) {
+      alive = false
+    }
+    counter(nextPasswordUses)
+  }
 
-	const selectNext = () => {
-		setBackgroundColor( '#EEEEEE')
-		setImage(wait)
-		console.log('next', nextPassword)
-		if (currentPasswordUses > nextPasswordUses) {
-			alive = false
-		}
-		counter(nextPasswordUses)
-	}
+  const selectNext = () => {
+    setDisabled(true)
+    setBackgroundColor('#EEEEEE')
+    setImage(wait)
+    console.log('next', nextPassword)
+    if (currentPasswordUses > nextPasswordUses) {
+      alive = false
+    }
+    counter(nextPasswordUses)
+  }
 
-	return (
-	<Panel id={props.id}>
-		<PanelHeader
-			className="Header"
-			left={<PanelHeaderBack onClick={props.go} data-to="home" />}
-		>
-			Популярность паролей
-		</PanelHeader>
-		<Div className="FlexCenter PopularityBackground" style={{ backgroundColor: backgroundColor }}>
+  return (
+    <Panel id={ props.id }>
+      <PanelHeader
+        className="Header"
+        left={ <PanelHeaderBack
+          onClick={ props.go }
+          data-to="home"
+        /> }
+      >
+        Популярность паролей
+      </PanelHeader>
+      <Div
+        className="FlexCenter PopularityBackground"
+        style={ { backgroundColor: backgroundColor } }
+      >
 			<span>
 				Счёт: { score }
 			</span>
-			<p>
-				Выберите пароль, который по-вашему более популярный
-			</p>
-			<img
-				className="Thinker"
-				src={image}
-				alt="thinker"
-			/>
-			<Div className="ChooseButtons">
-				<Div className="Password CurrentPassword">
-					<Button
-						className="PasswordButton"
-						onClick={selectCurrent}>
-						{ currentPassword }
-					</Button>
-					{ currentPasswordUses } использований
-				</Div>
-				<span className="ButtonsSeparator" />
-				<Div className="Password NextPassword">
-					<Button
-						className="PasswordButton"
-						onClick={selectNext}>
-						{ nextPassword }
-					</Button>
-					{ nextMask } использований
-				</Div>
-			</Div>
-		</Div>
-	</Panel>
-	)
+        <p>
+          Выберите пароль, который по-вашему более популярный
+        </p>
+        <img
+          className="Thinker"
+          src={ image }
+          alt="thinker"
+        />
+        <Div className="ChooseButtons">
+          <Div className="Password CurrentPassword">
+            <Button
+              disabled={disabled}
+              className="PasswordButton"
+              onClick={ selectCurrent }
+            >
+              { currentPassword }
+            </Button>
+            { currentPasswordUses } использований
+          </Div>
+          <span className="ButtonsSeparator" />
+          <Div className="Password NextPassword">
+            <Button
+              disabled={disabled}
+              className="PasswordButton"
+              onClick={ selectNext }
+            >
+              { nextPassword }
+            </Button>
+            { nextMask } использований
+          </Div>
+        </Div>
+      </Div>
+    </Panel>
+  )
 }
 
 Popularity.propTypes = {
-	id: PropTypes.string.isRequired,
-	go: PropTypes.func.isRequired,
-	bestScore: PropTypes.number,
-	setBestScore: PropTypes.func
+  id: PropTypes.string.isRequired,
+  go: PropTypes.func.isRequired,
+  bestScore: PropTypes.number,
+  setBestScore: PropTypes.func
 }
 
 export default Popularity
